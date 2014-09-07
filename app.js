f528d1eb2d0686d1c47a75787c97e151a55cae45
Kartik Talwar <hi@kartikt.com>
2014-09-06 23:59:47 -0700

    Techcrunch Disrekt

--- app.js
+++ app.js
@@ -4,13 +4,26 @@ require.config({
 });
 
 
-require(['jquery'], function($) {
+var scripts = [
+                'jquery',
+                'codeblock/vendor/ace/ace',
+                'codeblock/vendor/ace/theme-dawn',
+                'codeblock/vendor/ace/mode-javascript',
+                'codeblock/vendor/jquery-textrange',
+                'codeblock/js/linked-editor'
+              ];
+
+
+
+require(scripts, function($) {
 
   console.log('here yo');
+  $('pre').each(function () {
+    $(this).codeblock();
+  });
 
-   var request = $.ajax({ type: 'GET', url: encodeURI('https://api.uwaterloo.ca/v2/weather/current.json'), async : false });
-   console.log(request.responseText)
-
-   eval("alert(10)");
+   // var request = $.ajax({ type: 'GET', url: encodeURI('https://api.uwaterloo.ca/v2/weather/current.json'), async : false });
+   // console.log(request.responseText)
+   // eval("alert(10)");
 
 });
