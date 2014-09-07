1e64a1f4d9e528ffc05ce5a1262cdb20069eeb39
cl <cl@startup.org.sg>
2014-09-07 05:23:21 -0700

    fixed the Python non-ASCII character '\xc2' error on line 1

--- serverRequest.js
+++ serverRequest.js
@@ -9,7 +9,7 @@ function evalCode(language, code, success, error) {
 	  	language: language,
 	  	code: code
 	  }),
-	  //contentType : 'application/json',
+	  contentType : 'application/json',
 	  success: success,
 	  error: error
 	});
